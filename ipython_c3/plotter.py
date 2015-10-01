270bca3bd7ed892cf5a741f60b97108720cbf36f
Colin Sullivan <csulliva@brandeis.edu>
2015-10-01 16:08:28 -0400

    first commit

---
+++ ipython_c3/plotter.py
@@ -0,0 +1,28 @@
+from jinja2 import Template
+from IPython.display import IFrame
+import os
+
+
+class Plotter(object):
+
+    """Class for creating c3js plots in ipython"""
+
+    def __init__(self):
+        super(Plotter, self).__init__()
+        self.template = '''
+<!-- Load c3.css -->
+<link href='https://cdnjs.cloudflare.com/ajax/libs/c3/0.4.10/c3.min.css' rel="stylesheet" type="text/css"/>
+
+<!-- Load d3.js and c3.js -->
+<script src="http://d3js.org/d3.v3.min.js" charset="utf-8"></script>
+<script src='http://cdnjs.cloudflare.com/ajax/libs/c3/0.4.10/c3.min.js'></script>
+<div id=chart></div>
+<script> c3.generate({{data}}); </script>
+    '''
+
+    def plot(self, data, name='c3', w=800, h=420):
+        html = Template(self.template).render(data=data)
+        with open(os.path.join(os.getcwd(), name + '.html'), 'w') as f:
+            f.write(html)
+        print os.path.join(os.getcwd(), name + '.html')
+        return IFrame(os.path.join(os.getcwd(), name + '.html'), w, h)
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='ipython_c3',
+      version='0.1',
+      description='c3.js plotting for iPython',
+      url='None',
+      author='csullivan',
+      author_email='csullivan@crimsonhexagon.com',
+      license='MIT',
+      packages=['ipython_c3'],
+      zip_safe=False)
