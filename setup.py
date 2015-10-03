1c7fd8eca849528546027c317a4cbddf530673dd
Colin Sullivan <csulliva@brandeis.edu>
2015-10-03 12:37:42 -0400

    setup.py fix

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
-
+from setuptools import find_packages
 setup(name='iplotter',
       version='0.1',
       description='c3.js and plotly.js for iPython',
       url='https://github.com/niloch/iplotter',
-      author='csullivan',
+      author='Colin',
       author_email='csullivan@crimsonhexagon.com',
       license='MIT',
-      packages=['iplotter'],
+      packages=find_packages(),
       install_requires=[
           'Jinja2>=2.0',
           'ipython>=3.0',
