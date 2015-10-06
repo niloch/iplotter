8dc1c655786bd1cc5fbb8ac0a951bf5707f1e365
Colin Sullivan <csulliva@brandeis.edu>
2015-10-06 17:15:25 -0400

    autmatic versioning from git tags

--- setup.py
+++ setup.py
@@ -1,17 +1,26 @@
 from setuptools import setup
 from setuptools import find_packages
-setup(name='iplotter',
-      version='0.1',
-      description='C3.js and plotly.js for iPython/Jupyter notebooks',
-      url='https://github.com/niloch/iplotter',
-      author='Colin',
-      author_email='csulliva@brandeis.edu',
-      license='MIT',
-      packages=find_packages(),
-      install_requires=[
-          'Jinja2>=2.0',
-          'ipython>=3.0',
-          'notebook>=3.0'
-      ],
-      zip_safe=False,
-      include_package_data=True)
+import os
+
+
+def get_version():
+    version = os.popen('git describe', "r").read().strip()
+    return version
+
+if __name__ == '__main__':
+
+    setup(name='iplotter',
+          version=get_version(),
+          description='C3.js and plotly.js for iPython/Jupyter notebooks',
+          url='https://github.com/niloch/iplotter',
+          author='Colin',
+          author_email='csulliva@brandeis.edu',
+          license='MIT',
+          packages=find_packages(),
+          install_requires=[
+              'Jinja2>=2.0',
+              'ipython>=3.0',
+              'notebook>=3.0'
+          ],
+          zip_safe=False,
+          include_package_data=True)
