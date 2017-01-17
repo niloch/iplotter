5dc2b73d438757d1140b2ba185b01b4998cdadee
Colin Sullivan <csulliva@brandeis.edu>
2017-01-17 16:58:45 -0500

    missed relocating virtual browser class to its own module

--- iplotter/base_plotter.py
+++ iplotter/base_plotter.py
@@ -53,35 +53,3 @@ class IPlotter(object):
         plot in the notebook
         '''
         pass
-
-
-class VirtualBrowser(object):
-    """Helper class for converting html charts to png"""
-
-    def __init__(self, driver=webdriver.Chrome):
-        super(VirtualBrowser, self).__init__()
-        self.driver = driver()
-
-    def __enter__(self):
-        return self
-
-    def save_as_png(self, filename, width=300, height=250):
-        '''
-        open saved html file in an virtual browser and save a screen shot to PNG format
-        '''
-        self.driver.set_window_size(width, height)
-        self.driver.get('file://{path}/{filename}'.format(
-            path=os.getcwd(), filename=filename + ".html"))
-        time.sleep(1)
-        self.driver.save_screenshot(filename + ".png")
-
-    def __exit__(self, type, value, traceback):
-        self.driver.quit()
-        return True
-
-    def quit(self):
-        '''
-        shutdown virtual browser when finished
-        '''
-        self.driver.quit()
-        return True
