edf46167a0919430e361ed6d28de7754df209c05
Colin Sullivan <csulliva@brandeis.edu>
2017-03-31 14:31:15 -0400

    update chartjs library to 2.5 and refix scroll bar

--- README.md
+++ README.md
@@ -142,28 +142,28 @@ data = {
                "Cycling", "Running"],
     "datasets": [
         {
-            "label": "My First dataset",
-            "fillColor": "rgba(220,220,220,0.2)",
-            "strokeColor": "rgba(220,220,220,1)",
-            "pointColor": "rgba(220,220,220,1)",
-            "pointStrokeColor": "#fff",
-            "pointHighlightFill": "#fff",
-            "pointHighlightStroke": "rgba(220,220,220,1)",
-            "data": [65, 59, 90, 81, 56, 55, 40]
+            "label": "Trace 1",
+            "backgroundColor": "rgba(179,181,198,0.2)",
+            "borderColor": "rgba(179,181,198,1)",
+            "pointBackgroundColor": "rgba(179,181,198,1)",
+            "pointBorderColor": "#fff",
+            "pointHoverBackgroundColor": "#fff",
+            "pointHoverBorderColor": "rgba(179,181,198,1)",
+            "data": [65, 59, 30, 81, 56, 55, 40]
         }, {
-            "label": "My Second dataset",
-            "fillColor": "rgba(151,187,205,0.2)",
-            "strokeColor": "rgba(151,187,205,1)",
-            "pointColor": "rgba(151,187,205,1)",
-            "pointStrokeColor": "#fff",
-            "pointHighlightFill": "#fff",
-            "pointHighlightStroke": "rgba(151,187,205,1)",
+            "label": "Trace 2",
+            "backgroundColor": "rgba(255,99,132,0.2)",
+            "borderColor": "rgba(255,99,132,1)",
+            "pointBackgroundColor": "rgba(255,99,132,1)",
+            "pointBorderColor": "#fff",
+            "pointHoverBackgroundColor": "#fff",
+            "pointHoverBorderColor": "rgba(255,99,132,1)",
             "data": [28, 48, 40, 19, 96, 27, 100]
         }
     ]
 }
 
-plotter.plot(data, chart_type="Radar", w=500, h=500)
+plotter.plot_and_save(data, 'radar', options=None)
 ```
 ![Plot4](https://github.com/niloch/iplotter/blob/master/imgs/plot4.png?raw=true "Plot 4")
 
--- iplotter/chartjs_plotter.py
+++ iplotter/chartjs_plotter.py
@@ -12,23 +12,30 @@ class ChartJSPlotter(IPlotter):
 
     head = '''
         <!-- Load Charts.js -->
-       <script src='https://cdnjs.cloudflare.com/ajax/libs/Chart.js/1.0.2/Chart.min.js'></script>
+       <script src='https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.5.0/Chart.bundle.min.js'></script>
     '''
 
     template = '''
         <canvas id='{{div_id}}'></canvas>
         <script>
             var ctx = document.getElementById('{{div_id}}').getContext('2d');
-            ctx.canvas.width  = window.innerWidth - (.05 * window.innerWidth);
-            ctx.canvas.height = window.innerHeight - (.05 * window.innerHeight);
-            var myNewChart = new Chart(ctx).{{chart_type}}({{data}});
+            ctx.canvas.width  = {{w}} - (.1 * {{w}});
+            ctx.canvas.height = {{h}} - (.15 * {{h}});
+            var myNewChart = new Chart(ctx,{ type: '{{chart_type}}', data: {{data}}, options: {{options}} });
         </script>
     '''
 
     def __init__(self):
         super(ChartJSPlotter, self).__init__()
 
-    def render(self, data, chart_type, div_id="chart", head=""):
+    def render(self,
+               data,
+               chart_type,
+               options=None,
+               div_id="chart",
+               head="",
+               w=800,
+               h=420):
         '''
         render the data in HTML template
         '''
@@ -41,11 +48,16 @@ class ChartJSPlotter(IPlotter):
             div_id=div_id.replace(" ", "_"),
             data=json.dumps(
                 data, indent=4).replace("'", "\\'").replace('"', "'"),
-            chart_type=chart_type)
+            chart_type=chart_type,
+            options=json.dumps(
+                options, indent=4).replace("'", "\\'").replace('"', "'"),
+            w=w,
+            h=h)
 
     def plot_and_save(self,
                       data,
                       chart_type,
+                      options=None,
                       w=800,
                       h=420,
                       filename='chart',
@@ -53,26 +65,44 @@ class ChartJSPlotter(IPlotter):
         '''
         save the rendered html to a file and return an IFrame to display the plot in the notebook
         '''
-        self.save(data, chart_type, filename, overwrite)
+        self.save(data, chart_type, options, filename, w, h, overwrite)
         return IFrame(filename + '.html', w, h)
 
-    def plot(self, data, chart_type, w=800, h=420):
+    def plot(self, data, chart_type, options=None, w=800, h=420):
         '''
         output an iframe containing the plot in the notebook without saving
         '''
         return HTML(
             self.iframe.format(
                 source=self.render(
-                    data=data, chart_type=chart_type, head=self.head),
+                    data=data,
+                    chart_type=chart_type,
+                    options=options,
+                    head=self.head,
+                    w=w,
+                    h=h),
                 w=w,
                 h=h))
 
-    def save(self, data, chart_type, filename='chart', overwrite=True):
+    def save(self,
+             data,
+             chart_type,
+             options=None,
+             filename='chart',
+             w=800,
+             h=420,
+             overwrite=True):
         '''
         save the rendered html to a file in the same directory as the notebook
         '''
         html = self.render(
-            data=data, chart_type=chart_type, div_id=filename, head=self.head)
+            data=data,
+            chart_type=chart_type,
+            options=options,
+            div_id=filename,
+            head=self.head,
+            w=w,
+            h=h)
 
         if overwrite:
             with open(filename.replace(" ", "_") + '.html', 'w') as f:
